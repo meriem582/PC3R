056f46f8ef03beb10298326e7b8f12f6b146a910
meriem <meriembenaissa066@email.com>
2025-04-29 21:41:11 +0200

    examens update

--- PROJETPC3R/strapi-server/src/api/track/controllers/track.ts
+++
@@ -1,7 +0,0 @@
-/**
- * track controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::track.track');
--- PROJETPC3R/strapi-server/src/api/track/routes/track.ts
+++
@@ -1,7 +0,0 @@
-/**
- * track router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::track.track');
